d6b58457940cb6afc310977f6b1d75d0904bfb3e
Davi <davi.frrs@outlook.com>
2023-04-29 14:57:35 -0300

    feat: Home page

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,53 @@
+import { ExerciseCard } from "@components/ExerciseCard";
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack, Center, Heading, Text } from "native-base";
+import { VStack, Center, FlatList, Text, HStack, Heading } from "native-base";
+import { useState } from "react";
 
 
 
 export function Home() {
+    const [groups, setGroups] = useState(['costa', 'ombro', 'bíceps', 'tríceps'])
+    const [exercises, setExercises] = useState(['Puxada frontal', 'Remada Curvada', 'Remada Unilateral', 'Levantamento Terra']);
+    const [selectedGroup, setSelectedGroup] = useState('costa');
+
     return (
         <VStack flex={1}>
             <HomeHeader />
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <Group
+                        name={item}
+                        isActive={selectedGroup === item}
+                        onPress={() => setSelectedGroup(item)}
+                    />
+                )}
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                _contentContainerStyle={{ px: 8 }}
+                my={10}
+                maxH={10}
+            />
+
+            <VStack flex={1} px={8}>
+                <HStack justifyContent="space-between" mb={5}>
+                    <Heading color="gray.200" fontSize="md">Exercícios</Heading>
+                    <Text color="gray.200" fontSize="sm">4</Text>
+                </HStack>
+
+                <FlatList
+                    data={exercises}
+                    keyExtractor={item => item}
+                    renderItem={({ item }) => (
+                        <ExerciseCard />
+                    )}
+                    showsVerticalScrollIndicator={false}
+                    _contentContainerStyle={{ paddingBottom: 20 }}
+                />
+
+            </VStack>
         </VStack>
     )
 }
