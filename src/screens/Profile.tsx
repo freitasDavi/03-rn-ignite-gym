0de8e54484a25527c936c1ece0b2f03bb054edd1
Davi <davi.frrs@outlook.com>
2023-05-01 13:24:05 -0300

    feat: profile screen

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,85 @@
-import { Center, Text } from "native-base";
+import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { useState } from "react";
+import { TouchableOpacity } from "react-native";
 
-
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+    const [photoIsLoading, setPhotoIsLoading] = useState(false);
+
     return (
-        <Center flex={1}>
-            <Text color="white">Profile</Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader
+                title="Perfil"
+            />
+            <ScrollView>
+                <Center mt={6} px={10}>
+                    {photoIsLoading ? (
+                        <Skeleton
+                            w={PHOTO_SIZE}
+                            h={PHOTO_SIZE}
+                            rounded="full"
+                            startColor="gray.500"
+                            endColor="gray.300"
+                        />
+
+                    ) : (
+                        <UserPhoto
+                            source={{ uri: "https://github.com/freitasDavi.png" }}
+                            alt="Foto do usuário"
+                            size={PHOTO_SIZE}
+                        />
+                    )}
+                    <TouchableOpacity>
+                        <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+                            Alterar foto
+                        </Text>
+                    </TouchableOpacity>
+
+                    <Input
+                        bg="gray.600"
+                        placeholder="Nome"
+                    />
+
+                    <Input
+                        bg="gray.600"
+                        value="davi@gmail.com"
+                        isDisabled
+                    />
+                </Center>
+                <VStack px={10} mt={12} mb={9}>
+                    <Heading fontSize="md" color="gray.200" mb={2}>
+                        Alterar senha
+                    </Heading>
+
+                    <Input
+                        bg="gray.600"
+                        placeholder="Senha antiga"
+                        secureTextEntry
+                    />
+
+                    <Input
+                        bg="gray.600"
+                        placeholder="Nova senha"
+                        secureTextEntry
+                    />
+
+                    <Input
+                        bg="gray.600"
+                        placeholder="Confirme a nova senha"
+                        secureTextEntry
+                    />
+
+                    <Button
+                        title="Atualizar"
+                        mt={4}
+                    />
+                </VStack>
+            </ScrollView>
+        </VStack>
     )
 }
