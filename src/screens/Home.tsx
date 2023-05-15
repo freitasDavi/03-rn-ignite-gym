6f11c3c57fd895df68137cc76ccf29f5f5cdc79e
Davi <davi.frrs@outlook.com>
2023-05-14 21:50:15 -0300

    home page made

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,19 +1,22 @@
 import { ExerciseCard } from "@components/ExerciseCard";
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { useNavigation } from "@react-navigation/native";
+import { Loading } from "@components/Loading";
+import { ExerciseDTO } from "@dtos/ExerciseDTO";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
 import { api } from "@services/api";
 import { AppError } from "@utils/AppError";
 import { VStack, Center, FlatList, Text, HStack, Heading, useToast } from "native-base";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 
 
 export function Home() {
+    const [isLoading, setIsLoading] = useState(true);
     const [groups, setGroups] = useState<string[]>([])
-    const [exercises, setExercises] = useState(['Puxada frontal', 'Remada Curvada', 'Remada Unilateral', 'Levantamento Terra']);
-    const [selectedGroup, setSelectedGroup] = useState('costa');
+    const [exercises, setExercises] = useState<ExerciseDTO[]>([]);
+    const [selectedGroup, setSelectedGroup] = useState('costas');
     const toast = useToast();
     const navigation = useNavigation<AppNavigatorRoutesProps>();
 
@@ -23,6 +26,7 @@ export function Home() {
 
     async function fetchGroups() {
         try {
+            setIsLoading(true);
             const response = await api.get("/groups");
 
             setGroups(response.data);
@@ -36,12 +40,39 @@ export function Home() {
                 placement: 'top',
                 bgColor: 'red.500'
             })
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    async function fetchExercisesByGroup() {
+        try {
+            setIsLoading(true);
+            const response = await api.get(`/exercises/bygroup/${selectedGroup}`);
+
+            setExercises(response.data);
+
+        } catch (error) {
+            const isAppError = error instanceof AppError;
+            const title = isAppError ? error.message : 'Não foi possível carregar os exercícios.'
+
+            toast.show({
+                title,
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+        } finally {
+            setIsLoading(false);
         }
     }
 
     useEffect(() => {
         fetchGroups();
     }, []);
+
+    useFocusEffect(useCallback(() => {
+        fetchExercisesByGroup();
+    }, [selectedGroup]));
 
     return (
         <VStack flex={1}>
@@ -64,25 +95,32 @@ export function Home() {
                 minH={10}
             />
 
-            <VStack flex={1} px={8}>
-                <HStack justifyContent="space-between" mb={5}>
-                    <Heading color="gray.200" fontSize="md" fontFamily="heading">Exercícios</Heading>
-                    <Text color="gray.200" fontSize="sm">4</Text>
-                </HStack>
+            {isLoading ? (
+                <Loading />
+            ) : (
 
-                <FlatList
-                    data={exercises}
-                    keyExtractor={item => item}
-                    renderItem={({ item }) => (
-                        <ExerciseCard
-                            onPress={handleOpenExerciseDetails}
-                        />
-                    )}
-                    showsVerticalScrollIndicator={false}
-                    _contentContainerStyle={{ paddingBottom: 20 }}
-                />
+                <VStack flex={1} px={8}>
+                    <HStack justifyContent="space-between" mb={5}>
+                        <Heading color="gray.200" fontSize="md" fontFamily="heading">Exercícios</Heading>
+                        <Text color="gray.200" fontSize="sm">{exercises.length}</Text>
+                    </HStack >
 
-            </VStack>
-        </VStack>
+                    <FlatList
+                        data={exercises}
+                        keyExtractor={item => item.id}
+                        renderItem={({ item }) => (
+                            <ExerciseCard
+                                data={item}
+                                onPress={handleOpenExerciseDetails}
+                            />
+                        )}
+                        showsVerticalScrollIndicator={false}
+                        _contentContainerStyle={{ paddingBottom: 20 }}
+                    />
+
+                </VStack >
+            )
+            }
+        </VStack >
     )
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,15 +2,16 @@ import { AppError } from "@utils/AppError";
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://192.168.15.103:3333"
+    // 192.168.15.103
+    baseURL: "http://192.168.0.108:3333"
 })
 
 api.interceptors.response.use(response => response, (error) => {
     if (error.response && error.response.data) {
-        return Promise.reject(new AppError(error.response.data.message ));
+        return Promise.reject(new AppError(error.response.data.message));
     }
 
     return Promise.reject(error);
 });
 
-export { api}
+export { api }
